6d44711df6f42f123c1c3a06d236b940d3dacdf1
mariamaqsood <mariamaqsood2020@namal.edu.pk>
2022-09-28 23:08:54 +0500

    cal03.py code

---
+++ calculator/cal03.py
@@ -0,0 +1,16 @@
+#calculator in python(v0.3)
+
+def add (num1 , num2):
+return num1 + num2
+
+def sub (num1 , num2):
+return num1 - num2
+
+def mul (num1 , num2):
+return num1 * num2
+
+#main
+res = add (5,3)
+print(res)
+print(sub(5,3))
+print(mul(5,3))
