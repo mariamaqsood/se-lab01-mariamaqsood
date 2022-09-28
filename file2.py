40630bb2921ef574ffdfde2e25ddf033ea1f8cc4
irfan786khan <irfan2020@namal.edu.pk>
2022-09-28 23:33:07 +0500

    this is my file

---
+++ file2.py
@@ -0,0 +1,20 @@
+# calculator in Python(v0.3)
+
+def add(num1, num2):
+	return num1 + num2
+
+def sub(num1, num2)
+	Return num1 - num2
+
+def mul(num1, num2)
+return num1 * num2
+def div(n,n2)
+return n//n2
+def div(n,n2)
+return n**0.5,n**0.5
+
+# main
+res = add(5, 3)
+print(res)
+print(sub(5,3))
+print(mul(5,3))
